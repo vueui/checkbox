5cec1b8f4d5f3d0bf145a68fa4f62a05a399e6eb
Agon Bina <agon_bina@hotmail.com>
2015-08-26 00:43:37 +0200

    first implementation

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 
-module.exports = function(Vue) {
-	Vue.component('ui-checkbox', {
-		template: '<input type="" tabindex="0" class="ui checkbox" />'
-	})
+import tpl from './template.jade'
+
+let definition = {
+	template: tpl,
+	props: {
+		label: {
+			type: String,
+			required: true
+		},
+		model: {
+			twoWay: true
+		},
+		type: {
+			type: String,
+			validator(value) {
+				return value === 'slider' || value === 'toggle'
+			}
+		}
+	},
+	compiled() {
+		console.log('Before compile...')
+		console.log(this.$el)
+	}
 }
+
+export default Vue => Vue.component('ui-checkbox', definition)
